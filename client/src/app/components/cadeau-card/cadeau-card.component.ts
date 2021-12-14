34ff21fd0fcc0d29a49a510d6e9ec56a5465ac76
Valentin MASSE <valmasse@gmx.fr>
2021-12-14 11:47:17 +0100

    creation of first components

---
+++ client/src/app/components/cadeau-card/cadeau-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadeau-card',
+  templateUrl: './cadeau-card.component.html',
+  styleUrls: ['./cadeau-card.component.css']
+})
+export class CadeauCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
