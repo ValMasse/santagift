81ce628681355467f0af76bebaf06dcb0d00f3db
Valentin MASSE <valmasse@gmx.fr>
2021-12-14 15:03:14 +0100

    add html code for display a gift in a bootstrap card

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadeauxComponent } from './pages/cadeaux/cadeaux.component';
 import { ErrorComponent } from './pages/error/error.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'listGifts', component: CadeauxComponent},
   { path: 'not-found', component: ErrorComponent },
   { path: '**', redirectTo: 'not-found' }
 ];
