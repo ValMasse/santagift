82e9020ed935af214e943ea5abec1dfd7798595d
Valentin MASSE <valmasse@gmx.fr>
2021-12-14 14:00:57 +0100

    [front]add header on all pages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadeauxComponent } from './pages/cadeaux/cadeaux.component';
+import { CadeauCardComponent } from './components/cadeau-card/cadeau-card.component';
+import { CadeauListComponent } from './components/cadeau-list/cadeau-list.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HeaderComponent } from './pages/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadeauxComponent,
+    CadeauCardComponent,
+    CadeauListComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
