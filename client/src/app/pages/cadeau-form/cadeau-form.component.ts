ba05909d3d108601111f36aecfabd858165b2f27
Valentin MASSE <valmasse@gmx.fr>
2021-12-17 11:51:31 +0100

    work on cadeau form

--- client/src/app/pages/cadeau-form/cadeau-form.component.ts
+++ client/src/app/pages/cadeau-form/cadeau-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadeau-form',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadeauFormComponent implements OnInit {
 
-  constructor() { }
+  cadeauForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.cadeauForm = this.formBuilder.group({
+      
+    })
+   }
+
 
   ngOnInit(): void {
   }
